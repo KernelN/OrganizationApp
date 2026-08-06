333c4eccddaf900a955ce931809fe3c7d48c05c4
AlejandroInsausti <alejandroninsausti@gmail.com>
2026-08-05 21:09:39 -0300

    Clean old app

--- src/utils/ulid.js
+++
@@ -1,9 +0,0 @@
-import { ulid as generateUlid } from 'ulid';
-
-/**
- * Generate a timestamp-sortable, 26-character URL-safe unique identifier.
- * @returns {string} ULID string
- */
-export function ulid() {
-  return generateUlid();
-}
